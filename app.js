c61da7bb0c81092c3fb5fb9f65d4ccd843b7e0cc
SebastianBuritica <93333842+SebastianBuritica@users.noreply.github.com>
2022-02-23 19:54:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     let width = 10
+    let bombAmount = 20
     let squares = []
+    
 
     //Create Board
     function createBoard() {
+        //get shuffled game array with random bombs
+        const bombsArray = Array(bombAmount).fill('bomb')
+        const emptyArray = Array(width*width - bombAmount).fill('valid')
+        console.log(bombsArray)
+        console.log(emptyArray)
+        const gameArray = emptyArray.concat(bombsArray)
+        console.log(gameArray)
+
         for (let i = 0; i < width*width; i++) {
             const square = document.createElement('div')
             square.setAttribute('id', i)
             grid.appendChild(square)
-            
-
-            
+            squares.push(square)
         }
     }
+    
+    createBoard()
+
 })
